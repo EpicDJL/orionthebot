534587682ca1e5a34803fea7fd56cc20cb58c4f9
EpicDJL <awesomeriptide@gmail.com>
2017-01-15 16:35:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 var Discord = require("discord.js");
 var bot = new Discord.Client();
 var responseObject = {
